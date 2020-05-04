52b86783fa3b964f5f71fa0e757cb3c60ad2366c
Lydia Gregory <ljg2gb@virginia.edu>
2020-05-04 13:38:40 -0600

    pair programming added index and show action to user-controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+    def index
+        if 
+        @users = User.all
+        render json: @users
+    end
+
+    def show
+        @user = User.find(params[:name])
+        if @user 
+            render json: @user, include: [:groups]
+        else 
+            render json: {message: "We don't have user that matches this name in our database. Try again or create a new user."}
+        end
+    end
 end
