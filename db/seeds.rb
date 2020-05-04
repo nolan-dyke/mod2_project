e30e6412e018a9f6547b9a0efd542b19ac2fe6e5
Lydia Gregory <ljg2gb@virginia.edu>
2020-05-04 15:05:46 -0600

    adds user controller actions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Joiner.destroy_all
+Thought.destroy_all
+Content.destroy_all
+User.destroy_all
+Group.destroy_all
+
+nolan = User.create(name: "Nolan")
+lydia = User.create(name: "Lydia")
+
+meetup = Group.create(name: 'Go to meetups', description:'where we convince each other to go to at least one meetup while at Flatiron school',)
+
+joiner1 = Joiner.create(user: nolan, group: meetup)
+joiner2 = Joiner.create(user: lydia, group: meetup)
+
+content1 = Content.create(message: 'we love meetups', user: nolan, group: meetup)
+
+thought1 = Thought.create(comment: 'Great idea, Nolan', content: content1, user: lydia)
