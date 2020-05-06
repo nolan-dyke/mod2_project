89e2a817294c87d008ebd39edc7decb1bc9fce3c
Lydia Gregory <ljg2gb@virginia.edu>
2020-05-06 13:29:59 -0600

    adds create method to joiner controller

--- app/controllers/joiners_controller.rb
+++ app/controllers/joiners_controller.rb
@@ -4,5 +4,13 @@ class JoinersController < ApplicationController
         render json: @joiners
     end
 
+    def create
+        @joiner = Joiner.create(
+            user_id: params[:user_id],
+            group_id: params[:group_id]
+        )
+        redirect_to "http://localhost:3001/group.html?id=#{params[:group_id]}&user=#{params[:current_user]}"
+    end
+
 
 end
