8f5f2256624f7253dd9692ef8a3d101db5f6a6af
Lydia Gregory <ljg2gb@virginia.edu>
2020-05-06 22:29:13 -0600

    adds actions to Status Class

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -1,2 +1,30 @@
 class StatesController < ApplicationController
+    def index
+        @states = State.all
+        render json: @states
+    end
+
+    def show
+        @state = State.find(params[:id])
+        render json: @state
+    end
+
+    def update
+        @state = State.find(params[:id])
+        @state.update(
+            status: params[:status],
+            user_id: params[:user_id],
+            content_id: params[:content_id]
+        )
+        render json: @state
+    end
+
+    def create
+        @state = State.create(
+            status: params[:status],
+            user_id: params[:user_id],
+            content_id: params[:content_id]
+        )
+        render json: @state
+    end
 end
